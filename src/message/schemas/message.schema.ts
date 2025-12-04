1dbce87544892e5a2601e129bc5a771eb1575700
Ahmed Emad <159528322+ahmed-elgaml11@users.noreply.github.com>
2025-12-04 19:51:58 +0200

    add conservation and messages schema

---
+++ src/message/schemas/message.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+import { MediaType } from "src/_cores/global/class";
+import type { ConversationDocument } from "src/conversation/schemas/conversation.schema";
+import type { UserDocument } from "src/users/schemas/user.schema";
+
+
+export type MessageDocument = HydratedDocument<Message>
+
+@Schema({ timestamps: true })
+export class Message {
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Conversation' })
+    conversation: ConversationDocument
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+    sender: UserDocument
+
+    @Prop()
+    text?: string
+
+    @Prop()
+    mediaFiles?: MediaType[]
+
+    @Prop({ type: Boolean, default: false })
+    isDeleted?: boolean
+
+    @Prop({ type: [mongoose.Schema.Types.ObjectId], ref: 'User' })
+    seenBy?: UserDocument[]
+}
+
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
